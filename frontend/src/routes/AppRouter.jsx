f5965f837991c9a0d9b6778768d3dd4384a0fbf5
unknown <asimmunnir1@gmail.com>
2026-04-18 23:55:31 +0500

    Create Bottom Navigation

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -8,6 +8,7 @@ import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin'
 import Home from '../pages/general/Home'
 import CreateFood from '../pages/food-partner/CreateFood'
 import Profile from '../pages/food-partner/Profile'
+import BottomNav from '../components/BottomNav'
 
 const AppRouter = () => {
     return (
@@ -19,7 +20,7 @@ const AppRouter = () => {
                     <Route path='/user/login' element={<UserLogin />} />
                     <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
                     <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
-                    <Route path="/home" element={<Home />} />
+                    <Route path="/home" element={<><Home /><BottomNav /></>} />
                     <Route path="/create-food" element={<CreateFood />} />
                     <Route path="/food-partner/:id" element={<Profile />} />
                 </Routes>
